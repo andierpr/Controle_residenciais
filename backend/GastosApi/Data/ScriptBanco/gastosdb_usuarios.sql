afad4938106e5ac48abd65e8ea4777ef73202250
Andier Prattes Ramos <andier@agafarma.com.br>
2025-12-17 15:13:20 -0300

    Commit_sceiptBD

--- backend/GastosApi/Data/ScriptBanco/gastosdb_usuarios.sql
+++ backend/GastosApi/Data/ScriptBanco/gastosdb_usuarios.sql
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `usuarios`
 --
-
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
